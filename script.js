c59fbb31ce85175b67445927b6840863ff844f57
roninhaf <roninhaf@gmail.com>
2019-01-30 20:48:22 -0500

    set transition duration to 0s if rotation 90deg.

--- script.js
+++ script.js
@@ -3,26 +3,25 @@
 const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
-
+const hand = Array.from(document.querySelectorAll('.second-hand,.min-hand,.hour-hand'));
 
 function clockDisplay(){
     
     const getTime = new Date();
     
     const seconds = getTime.getSeconds();
-    console.log(seconds);
     const secondInDegree = (seconds/60)*360+90;
     secondHand.style.transform = `rotate(${secondInDegree}deg)`;
 
     const min = getTime.getMinutes();
-    console.log(min);
     const minInDegree = (min/60)*360+90;
     minHand.style.transform = `rotate(${minInDegree}deg)`;
 
     const hour = getTime.getHours();
-    console.log(hour);
     const hourInDegree = (hour/12)*360+90;
     hourHand.style.transform = `rotate(${hourInDegree}deg)`;
+    
+    hand.forEach(a=>a.style.transform == "rotate(90deg)"?a.style.transitionDuration = "0s":a.style.transitionDuration = "0.2s");
 }
 
 setInterval(clockDisplay,1000);
