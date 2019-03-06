f1e3272dddfb56917dc2065a3fc7090b7c44beb3
roninhaf <roninhaf@gmail.com>
2019-03-06 15:57:57 -0500

    Improved reading

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ function clockDisplay(){
     const getTime = new Date();
     
     const seconds = getTime.getSeconds();
-    const secondInDegree = (seconds/60)*360+90;
+    const secondInDegree = (seconds / 60) * 360 + 90;
     secondHand.style.transform = `rotate(${secondInDegree}deg)`;
 
     const min = getTime.getMinutes();
-    const minInDegree = (min/60)*360+90;
+    const minInDegree = (min / 60) * 360 + 90;
     minHand.style.transform = `rotate(${minInDegree}deg)`;
 
     const hour = getTime.getHours();
-    const hourInDegree = (hour/12)*360+90;
+    const hourInDegree = (hour / 12) * 360 + 90;
     hourHand.style.transform = `rotate(${hourInDegree}deg)`;
     
-    hand.forEach(a=>a.style.transform == "rotate(90deg)"?a.style.transitionDuration = "0s":a.style.transitionDuration = "0.2s");
+    hand.forEach(hand => hand.style.transform == "rotate(90deg)" ? hand.style.transitionDuration = "0s" : hand.style.transitionDuration = "0.2s");
 }
 
 setInterval(clockDisplay,1000);
